0b289af0209db7405d6cffb5dfcd720f1c4d1c36
hirarki wendri <ventus.kiki@gmail.com>
2022-08-10 13:11:56 +0700

    adding log in feature

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -1,9 +1,13 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getx_supabase_app/app/constants/supabase_const.dart';
+import 'package:getx_supabase_app/app/routes/app_pages.dart';
 
 class LoginController extends GetxController {
-  //TODO: Implement LoginController
+  var isLoading = false.obs;
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
 
-  final count = 0.obs;
   @override
   void onInit() {
     super.onInit();
@@ -15,6 +19,28 @@ class LoginController extends GetxController {
   }
 
   @override
-  void onClose() {}
-  void increment() => count.value++;
+  void onClose() {
+    emailController.dispose();
+    passwordController.dispose();
+  }
+
+  Future<void> _signIn() async {
+    isLoading.value = true;
+
+    final res = await supabase.auth.signIn(
+      email: emailController.text,
+      password: passwordController.text,
+    );
+    final err = res.error;
+    isLoading.value = false;
+
+    if (err != null) {
+      Get.snackbar("Error", "${err.message}");
+      return;
+    }
+
+    Get.offAllNamed(Routes.CHAT);
+  }
+
+  void onLoginCallback() => _signIn();
 }
--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:getx_supabase_app/app/constants/widget_const.dart';
+import 'package:getx_supabase_app/app/widgets/textfield_widget.dart';
 
 import '../controllers/login_controller.dart';
 
@@ -8,15 +10,31 @@ class LoginView extends GetView<LoginController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('LoginView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'LoginView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      appBar: AppBar(title: Text('Sign In')),
+      body: ListView(
+        padding: formPadding,
+        children: [
+          TextFieldWidget(
+            controller: controller.emailController,
+            label: "Email",
+            inputType: TextInputType.emailAddress,
+          ),
+          formSpacer,
+          TextFieldWidget(
+            controller: controller.passwordController,
+            label: "Password",
+            isObscure: true,
+          ),
+          formSpacer,
+          Obx(
+            () => ElevatedButton(
+              onPressed: controller.isLoading.value
+                  ? null
+                  : controller.onLoginCallback,
+              child: const Text('Log In'),
+            ),
+          ),
+        ],
       ),
     );
   }
